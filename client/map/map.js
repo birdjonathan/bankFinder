6a5289dce32792bfe6f7f47e6667c03639b137d0
Jonathan Bird <jonbird81@gmail.com>
2014-09-04 22:38:59 -0700

    Makes ajax call and renders address of first object in Bank Locations object

--- client/map/map.js
+++ client/map/map.js
@@ -9,6 +9,17 @@ angular.module('bankFinder.main.map', ['ui.router', 'ngMap'])
       controller: 'MapController'
     });
 })
-.controller('MapController', function ($scope) {
+.controller('MapController', function ($scope, $http) {
+  $http({method: 'GET', url: 'https://m.chase.com/PSRWeb/location/list.action?lat=40.147864&lng=-82.990959'}).
+  success(function(data, status, headers, config) {
+    console.log("This is my bank info object", data);
+    $scope.banks = data.locations;
+    // this callback will be called asynchronously
+    // when the response is available
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
   $scope.message = 'Inside Map State!';
 });
