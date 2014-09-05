a367ea5d132ef1483a28b026e154afd6a1689965
Jonathan Bird <jonbird81@gmail.com>
2014-09-05 14:19:53 -0700

    Locates user and places markers of banks nearby

--- client/map/map.js
+++ client/map/map.js
@@ -10,15 +10,66 @@ angular.module('bankFinder.main.map', ['ui.router'])
     });
 })
 .controller('MapController', function ($scope, $http) { 
+  var siberia = new google.maps.LatLng(60, 105);
+  var browserSupportFlag =  new Boolean();
+
   $scope.message = 'Inside Map State!';
   
   var mapOptions = {
-        zoom: 14,
-        center: new google.maps.LatLng(37.790594, -122.417317),
+        zoom: 4,
+        center: new google.maps.LatLng(39.50, -98.35),
+        // center: new google.maps.LatLng(37.790594, -122.417317),
         mapTypeId: google.maps.MapTypeId.TERRAIN
     }
 
     $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+
+  // Try W3C Geolocation (Preferred)
+  if(navigator.geolocation) {
+    browserSupportFlag = true;
+    navigator.geolocation.getCurrentPosition(function(position) {
+      console.log("This is my position", position);
+      $scope.coordinates = position.coords;
+      console.log("this is my latitude", $scope.coordinates.latitude);
+      $scope.userLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      $scope.map.setCenter($scope.userLocation);
+      $scope.map.setZoom(14);
+
+    $http({method: 'GET', url: 'https://m.chase.com/PSRWeb/location/list.action?lat=' + $scope.coordinates.latitude + '&lng=' + $scope.coordinates.longitude}).
+    success(function(data, status, headers, config) {
+      console.log("This is my bank info object", data);
+      $scope.banks = data.locations;
+      console.log("This schould be an array of locations", $scope.banks);
+      
+      for (var i = 0; i < $scope.banks.length; i++){
+          placeMarkers($scope.banks[i]);
+      }
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+    });      
+    }, function() {
+      handleNoGeolocation(browserSupportFlag);
+    });
+  }
+  // Browser doesn't support Geolocation
+  else {
+    browserSupportFlag = false;
+    handleNoGeolocation(browserSupportFlag);
+  }
+
+  function handleNoGeolocation(errorFlag) {
+    if (errorFlag == true) {
+      alert("Geolocation service failed.");
+      $scope.userLocation = newyork;
+    } else {
+      alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+      $scope.userLocation = siberia;
+    }
+    $scope.map.setCenter($scope.userLocation);
+  }
 
     $scope.markers = [];
     
@@ -46,18 +97,4 @@ angular.module('bankFinder.main.map', ['ui.router'])
         e.preventDefault();
         google.maps.event.trigger(selectedMarker, 'click');
     }
-  $http({method: 'GET', url: 'https://m.chase.com/PSRWeb/location/list.action?lat=37.7833&lng=-122.4167'}).
-  success(function(data, status, headers, config) {
-    console.log("This is my bank info object", data);
-    $scope.banks = data.locations;
-    console.log("This schould be an array of locations", $scope.banks);
-    
-    for (var i = 0; i < $scope.banks.length; i++){
-        placeMarkers($scope.banks[i]);
-    }
-  }).
-  error(function(data, status, headers, config) {
-    // called asynchronously if an error occurs
-    // or server returns response with an error status.
-  });
 });
