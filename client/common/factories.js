81c027dd3f88800624d03188f70374dfd739dbce
Jonathan Bird <jonbird81@gmail.com>
2014-09-07 16:14:27 -0700

    Refactored to pass lat/long parameters from controller to factory

--- client/common/factories.js
+++ client/common/factories.js
@@ -8,8 +8,8 @@
     var getData = function(latitude, longitude, callback){
       return $http({
         method: 'GET',
-        // url: urlBase + latitude + '&lng' + longitude
-        url: 'https://m.chase.com/PSRWeb/location/list.action?lat=37.7526249&lng=-122.41368630000001'
+        url: urlBase + latitude + '&lng=' + longitude
+        // url: 'https://m.chase.com/PSRWeb/location/list.action?lat=37.7526249&lng=-122.41368630000001'
       })
       .then(function(response){
         console.log("Here is the factory response", response);
