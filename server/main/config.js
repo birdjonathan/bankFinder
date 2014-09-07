49ff844c65aa49bf987a2ebc6a6cc56f2101718a
Jonathan Bird <jonbird81@gmail.com>
2014-09-06 22:58:02 -0700

    Re-configured to use mongoose

--- server/main/config.js
+++ server/main/config.js
@@ -1,7 +1,6 @@
 "use strict";
 
 var mongoose    = require('mongoose'),
-    uriUtil     = require('mongodb-uri'),
     morgan      = require('morgan'),
     bodyParser  = require('body-parser'),
     middle      = require('./middleware');
@@ -10,8 +9,103 @@ var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }
 
 // I would use an environmental variable here for a production app
-var mongodbUri = 'mongodb://heroku_app29310252:4keu6dboi2ngtpqb5jt7nsbhm7@ds035260.mongolab.com:35260/heroku_app29310252'
-mongoose.connect(mongodbUri || 'mongodb://localhost/bankFinder');
+var uristring = 
+  process.env.MONGOLAB_URI ||
+  'mongodb://localhost/bankFinder';
+var html1 = '<title> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </title> \
+<head> \
+<style> body {color: #394a5f; font-family: sans-serif} </style> \
+</head> \
+<body> \
+<h1> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </h1> \
+See the <a href="https://devcenter.heroku.com/articles/nodejs-mongoose">supporting article on the Dev Center</a> to learn more about data modeling with Mongoose. \
+<br\> \
+<br\> \
+<br\> <h2> All Documents in MonogoDB database </h2> <pre><code> ';
+var html2 = '</code></pre> <br\> <i>';
+var html3 = ' documents. </i> <br\> <br\>';
+var html4 = '<h2> Queried (name.last = "Doe", age >64) Documents in MonogoDB database </h2> <pre><code> ';
+var html5 = '</code></pre> <br\> <i>';
+var html6 = ' documents. </i> <br\> <br\> \
+<br\> <br\> <center><i> Demo code available at <a href="http://github.com/mongolab/hello-mongoose">github.com</a> </i></center>';
+
+mongoose.connect(uristring, function(err, res){  
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+var userSchema = new mongoose.Schema({
+  name: {
+    first: String,
+    last: { type: String, trim: true }
+  },
+  age: { type: Number, min: 0}
+});
+
+// Compiles the schema into a model, opening (or creating, if
+// nonexistent) the 'PowerUsers' collection in the MongoDB database
+var PUser = mongoose.model('PowerUsers', userSchema);
+
+// Clear out old data
+PUser.remove({}, function(err) {
+  if (err) {
+    console.log ('error deleting old data.');
+  }
+});
+
+// Creating one user.
+var johndoe = new PUser ({
+  name: { first: 'John', last: '  Doe   ' },
+  age: 25
+});
+
+// Saving it to the database.  
+johndoe.save(function (err) {if (err) console.log ('Error on save!')});
+
+// Creating more users manually
+var janedoe = new PUser ({
+  name: { first: 'Jane', last: 'Doe' },
+  age: 65
+});
+janedoe.save(function (err) {if (err) console.log ('Error on save!')});
+
+// Creating more users manually
+var alicesmith = new PUser ({
+  name: { first: 'Alice', last: 'Smith' },
+  age: 45
+});
+alicesmith.save(function (err) {if (err) console.log ('Error on save!')});
+
+
+// In case the browser connects before the database is connected, the
+// user will see this message.
+var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
+
+app.get('/mongo', function(req, res) {
+
+ PUser.find({}).exec(function(err, result) { 
+    if (!err) { 
+      res.write(html1 + JSON.stringify(result, undefined, 2) +  html2 + result.length + html3);
+      // Let's see if there are any senior citizens (older than 64) with the last name Doe using the query constructor
+      var query = PUser.find({'name.last': 'Doe'}); // (ok in this example, it's all entries)
+      query.where('age').gt(64);
+      query.exec(function(err, result) {
+  if (!err) {
+    res.end(html4 + JSON.stringify(result, undefined, 2) + html5 + result.length + html6);
+  } else {
+    res.end('Error in second query. ' + err)
+  }
+      });
+    } else {
+      res.end('Error in first query. ' + err)
+    };
+  });
+});
+
+
 /*
  * Include all your global env variables here.
 */
