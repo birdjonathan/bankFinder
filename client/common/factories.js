a901562ef7b7a21e927d67401ffda90ea9c8a2b7
Jonathan Bird <jonbird81@gmail.com>
2014-09-07 16:56:15 -0700

    Refactored factory and controller for better error handling

--- client/common/factories.js
+++ client/common/factories.js
@@ -2,7 +2,7 @@
   'use strict';
   angular.module('bankFinder')
 
-  .factory('BankApiFactory', function($http){
+  .factory('BankApiFactory', function($http, $q){
     var urlBase = 'https://m.chase.com/PSRWeb/location/list.action?lat='
     var getData = {};
     var getData = function(latitude, longitude, callback){
@@ -11,10 +11,27 @@
         url: urlBase + latitude + '&lng=' + longitude
         // url: 'https://m.chase.com/PSRWeb/location/list.action?lat=37.7526249&lng=-122.41368630000001'
       })
-      .then(function(response){
-        console.log("Here is the factory response", response);
-        callback(response);
-      });
+      .then(
+        function(response){
+          console.log("Here is the factory response", response);
+          callback(response);
+        },
+        function(response){
+          // called when $http.get() failes
+          var errorMessage = "";
+          switch(response.status){
+              case 404:
+                  errorMessage = "Not found";
+                  break;
+              case 500:
+                  errorMessage = "Fatal error";
+                  break;
+              default:
+                  errorMessage = "An error has occurred";
+          }
+          callback(errorMessage);
+        }
+      );
     };
     return {
       'getData': getData
